704095a16bcdeda0e5552dad657dfe75700a9080
naoki5454 <naoking250912@icloud.com>
2021-05-15 14:30:15 +0000

    bootstrap3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,10 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
-
 //= require rails-ujs
+
+//= require jquery
+//= require jquery_ujs
 //= require activestorage
-//= require turbolinks
 //= require_tree .
+
