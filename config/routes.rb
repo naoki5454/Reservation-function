29f1e42a59ec92ab6566da8c39a94b28882a9d3e
naoki5454 <naoking250912@icloud.com>
2021-05-18 15:02:02 +0000

    error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :customers
+  devise_scope :customer do
+    get 'customers/sign_in' => 'customers/sessions#new', as: 'new_customer_session'
+    post 'customers/sign_in' => 'customers/sessions#create', as: 'customer_session'
+    get 'customers/sign_out' => 'customers/sessions#destroy', as: 'destroy_customer_session'
+    get 'customers/sign_up' => 'customers/registrations#new', as: 'new_customer_registration'
+    post 'customers' => 'customers/registrations#create', as: 'customer_registration'
+    get 'customers/password/new' => 'customers/passwords#new', as: 'new_customer_password'
+  end
   resources :reservationcs
   root 'homes#top'
 end
