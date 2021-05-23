714310ed3a11b421326a40bda32a5a7bda1b27f9
naoki5454 <naoking250912@icloud.com>
2021-05-23 15:01:03 +0000

    modelにメソッドの定義

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,8 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  def price_with_tax
+    1000 * 1.08
+  end
 end
