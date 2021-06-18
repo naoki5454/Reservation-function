796f279a31e9b89b0845c022cb2467a4e42f6fb2
naoki5454 <naoking250912@icloud.com>
2021-06-18 15:10:00 +0000

    save

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,14 @@
 //= require activestorage
 //= require_tree .
 
+ $(function () {
+    $("#nav-open").on("click", function () {
+      if ($(this).hasClass("active")) {
+        $(this).removeClass("active");
+        $("#nav-content").removeClass("open").fadeOut(100);
+      } else {
+        $(this).addClass("active");
+        $("#nav-content").fadeIn(100).addClass("open");
+      }
+    });
+  });
