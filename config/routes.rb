44b1f0914de1e9a4c78861734ea36873a0297cc6
naoki5454 <naoking250912@icloud.com>
2021-06-27 14:36:51 +0000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_scope :customer do
     get 'customers/sign_in' => 'customers/sessions#new', as: 'new_customer_session'
     post 'customers/sign_in' => 'customers/sessions#create', as: 'customer_session'
-    get 'customers/sign_out' => 'customers/sessions#destroy', as: 'destroy_customer_session'
+    delete 'customers/sign_out' => 'customers/sessions#destroy', as: 'destroy_customer_session'
     get 'customers/sign_up' => 'customers/registrations#new', as: 'new_customer_registration'
     post 'customers' => 'customers/registrations#create', as: 'customer_registration'
     get 'customers/password/new' => 'customers/passwords#new', as: 'new_customer_password'
