29451d3aa64bdb9fb480b8a399a2f135304bb0d8
naoki5454 <naoking250912@icloud.com>
2021-06-28 15:09:14 +0000

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     get 'customers/sign_up' => 'customers/registrations#new', as: 'new_customer_registration'
     post 'customers' => 'customers/registrations#create', as: 'customer_registration'
     get 'customers/password/new' => 'customers/passwords#new', as: 'new_customer_password'
-    get 'customers/edit' => 'customers#edit'
+    resources :customers, only: %i[edit update]
   end
   resources :reservationcs
   root 'homes#top'
