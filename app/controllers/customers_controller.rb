b6211ca77fd87e71a39fd60899075ab8261cbad2
naoki5454 <naoking250912@icloud.com>
2021-06-28 15:22:11 +0000

    edit

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,15 @@
-class CustomersController < ApplicationController
+class CustomersController < ApplicationControllers
     
     def edit
         @customer = current_customer
+    end
+    
+    def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer), notice: "編集成功しました。"
+    else
+      render :edit
+    end
     end
 end
