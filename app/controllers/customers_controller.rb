8950ae9017aade9f0a47ff8fda81398260ba2daf
naoki5454 <naoking250912@icloud.com>
2021-06-30 15:08:00 +0000

    フラッシュ

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -8,6 +8,7 @@ class CustomersController < ApplicationController
       @customer = current_customer
       if @customer.update(customer_params)
         redirect_to edit_customer_path(@customer)
+        flash[:notice] = '編集成功しました。'
       else
         render :edit
       end
